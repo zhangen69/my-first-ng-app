7ef723ae621cc95e996abfc450c3d4bb695353d8
Jacob <zhangen69@gmail.com>
2020-03-12 01:17:59 +0800

    reactive form

--- src/app/ng-forms/reactive-form/reactive-form.component.ts
+++ src/app/ng-forms/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
+  form: FormGroup;
+  formValue: any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    // this.form = new FormGroup({
+    //   username: new FormControl('', Validators.required),
+    //   password: new FormControl('', Validators.required),
+    //   dob: new FormControl(''),
+    // });
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      dob: [''],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  getControlsMessage() {
+    const controls = this.form.controls;
+    const controlMessages: any[] = [];
+
+    Object.keys(controls).forEach((key) => {
+      const control = controls[key];
+
+      controlMessages.push({
+        name: key,
+        dirty: control.dirty,
+        disabled: control.disabled,
+        // errors: control.errors,
+        invalid: control.invalid,
+        pristine: control.pristine,
+        status: control.status,
+        touched: control.touched,
+        untouched: control.untouched,
+        valid: control.valid,
+        value: control.value
+      });
+
+    });
+
+    return controlMessages;
+  }
+
+  onSubmit(formValue) {
+    this.formValue = formValue;
   }
 
 }
